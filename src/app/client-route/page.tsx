f282cef4a29e90f22b375aa148af62544b8938f9
denleonchev <den.leonchev@gmail.com>
2025-10-03 13:44:14 +0000

    use context

--- src/app/client-route/page.tsx
+++ src/app/client-route/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 
+import { useTheme } from "@/components/theme-provider";
+
 export default function ClientRoute() {
+  const theme = useTheme();
   return (
-    <h1>Client route</h1>
+    <h1 style={{
+      color: theme.colors.primary
+    }}>Client route themed</h1>
   );
 }
